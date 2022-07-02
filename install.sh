04406c7a92abe2642f00c28b640972b4f14a8a5a
TheGhostboi64 <99986496+TheGhostboi64@users.noreply.github.com>
2022-07-02 14:45:06 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,18 @@
 ghost64get () {
 curl --create-dirs -f -k -L -o "${2}" -S -s --insecure
 https://
+}
 
 clear 
 print "Choose a version to install:" choice
 case ${choice} in
+1) rude (clean) ;;
+2) rude (explict) ;;
+3) exit
+"Thank you for using this installer, if you have just installed the file(s), please start bash again, otherwise, reboot, just in case if it needs to reboot to save."
+popd > /dev/null
+
+			break;;
+	esac
+done
+}
